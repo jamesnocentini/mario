6db74b4511ecfb3be35452bced869c7fed4ec3d9
James Nocentini <james.nocentini@gmail.com>
2014-09-27 17:18:36 +0100

    initial commit

---
+++ mario/mario/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mario
+//
+//  Created by James Nocentini on 27/09/2014.
+//  Copyright (c) 2014 James Nocentini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
